2bbaf2760094372700f46341ad719933bf5bb5b0
truskavka <kozachuk.dz@gmail.com>
2022-10-04 09:54:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from kivymd.app import MDApp
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class MenuScreen(Screen):
+    pass
+
+
+class WordsScreen(Screen):
+    pass
+
+
+class IrregularVerbsScreen(Screen):
+    pass
+
+
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(WordsScreen(name='learnWords'))
+sm.add_widget(IrregularVerbsScreen(name='irregularV'))
+
+
+class DemoApp(MDApp):
+
+    def build(self):
+        screen = Builder.load_file('layout.kv')
+        return screen
+
+
+DemoApp().run()
