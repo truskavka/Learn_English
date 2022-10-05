fcb31fa08b998d8a9704fb93676958a15741a093
truskavka <kozachuk.dz@gmail.com>
2022-10-05 23:38:41 +0300

    Buttons now show word from dict

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -9,18 +11,40 @@ Window.size = (480, 800)
 
 
 x = {
-  "see": ["бачити", "see saw seen"]
+    "see": ["бачити", "see saw seen"],
+    "hear": ["чути", "hear heard heard"],
+    "touch": ["торкатися", ""],
+    "smell": ["нюхати", "smell smelt smelt"]
 }
 
 Json_example = json.dumps(x)
 
 
+# def get_list_of_keys(list_of_dicts):
+#     list_of_keys = list()
+#     for i in list_of_dicts:
+#         list_of_keys.append(i)
+#     print(list_of_keys)
+#     return list_of_keys
+
+
 class MenuScreen(Screen):
-    pass
+    name = 'menu'
+    button_name = 'Learn words'
 
 
 class WordsScreen(Screen):
     name = 'learnWords'
+    buttons_text = []
+
+    words_list = random.sample(list(x.keys()), k=4)
+
+    choice_button1_text = words_list[0]
+    choice_button2_text = words_list[1]
+    choice_button3_text = words_list[2]
+    choice_button4_text = words_list[3]
+
+    label_text = random.choice(words_list)
 
 
 class IrregularVerbsScreen(Screen):
@@ -45,4 +69,3 @@ class DemoApp(MDApp):
 
 
 DemoApp().run()
-
