a6087c3eec0badbcf565b987c9b0081d18b34159
truskavka <kozachuk.dz@gmail.com>
2022-10-06 19:30:49 +0300

    Buttons change color

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import random
 
+from googletrans import Translator
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.button import Button
 import json
 
 from kivy.core.window import Window
 
 Window.size = (480, 800)
-
 
 x = {
     "see": ["бачити", "see saw seen"],
@@ -19,13 +20,15 @@ x = {
 
 Json_example = json.dumps(x)
 
+translator = Translator()
 
-# def get_list_of_keys(list_of_dicts):
-#     list_of_keys = list()
-#     for i in list_of_dicts:
-#         list_of_keys.append(i)
-#     print(list_of_keys)
-#     return list_of_keys
+
+class Manager(ScreenManager):
+    def __init__(self):
+        super(Manager, self).__init__()
+        self.add_widget(MenuScreen(name='menu'))
+        self.add_widget(WordsScreen(name='learnWords'))
+        self.add_widget(IrregularVerbsScreen(name='irregularV'))
 
 
 class MenuScreen(Screen):
@@ -34,28 +37,33 @@ class MenuScreen(Screen):
 
 
 class WordsScreen(Screen):
-    name = 'learnWords'
-    buttons_text = []
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.name = 'learnWords'
+        words_list = random.sample(list(x.keys()), k=4)
 
-    words_list = random.sample(list(x.keys()), k=4)
+        self.choice_button1_text = words_list[0]
+        self.choice_button2_text = words_list[1]
+        self.choice_button3_text = words_list[2]
+        self.choice_button4_text = words_list[3]
 
-    choice_button1_text = words_list[0]
-    choice_button2_text = words_list[1]
-    choice_button3_text = words_list[2]
-    choice_button4_text = words_list[3]
+        self.correct_word = random.choice(words_list)
 
-    label_text = random.choice(words_list)
+        self.label_text = translator.translate(self.correct_word, dest="uk").text
+
+    def is_correct_answer(self, instance):
+        if instance.text == self.correct_word:
+            instance.text_color = 'green'
+            instance.line_color = 'green'
+            print(self)
+        else:
+            instance.text_color = 'red'
+            instance.line_color = 'red'
+
 
 
 class IrregularVerbsScreen(Screen):
     pass
-
-
-# Create the screen manager
-sm = ScreenManager()
-sm.add_widget(MenuScreen(name='menu'))
-sm.add_widget(WordsScreen(name='learnWords'))
-sm.add_widget(IrregularVerbsScreen(name='irregularV'))
 
 
 class DemoApp(MDApp):
